b0b687cd488b5f94577141d5b93fe45da6d72e97
Paul Bacchus <paulbacchus@M35-117.local>
2023-12-08 15:50:15 +0000

    Adding storage stack.

---
+++ stacks/StorageStack.ts
@@ -0,0 +1,17 @@
+import { Bucket, StackContext, Table } from "sst/constructs";
+
+export function StorageStack({ stack }: StackContext) {
+  // Create S3 bucket
+  const bucket = new Bucket(stack, "Uploads");
+
+  // Create DynamoDB table
+  const table = new Table(stack, "Notes", {
+    fields: {
+      userId: "string",
+      noteId: "string",
+    },
+    primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+  });
+
+  return { bucket, table };
+}
