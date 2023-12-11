94223913146e2cb8fe07ddbd6e7b693b6de89845
Paul Bacchus <paulbacchus@M35-117.local>
2023-12-11 15:31:21 +0000

    Securing the API

---
+++ packages/functions/src/delete.ts
@@ -0,0 +1,17 @@
+import { Table } from "sst/node/table";
+import handler from "@notes/core/handler";
+import dynamodb from "@notes/core/dynamodb";
+
+export const main = handler(async (event) => {
+  const params = {
+    TableName: Table.Notes.tableName,
+    Key: {
+      userId: event.requestContext.authorizer?.iam.cognitoIdentity.identityId,
+      noteId: event?.pathParameters?.id,
+    },
+  };
+
+  await dynamodb.delete(params);
+
+  return JSON.stringify({ status: true });
+});
---
+++ packages/functions/src/list.ts
@@ -0,0 +1,18 @@
+import { Table } from "sst/node/table";
+import handler from "@notes/core/handler";
+import dynamodb from "@notes/core/dynamodb";
+
+export const main = handler(async (event) => {
+  const params = {
+    TableName: Table.Notes.tableName,
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId":
+        event.requestContext.authorizer?.iam.cognitoIdentity.identityId,
+    },
+  };
+
+  const result = await dynamodb.query(params);
+
+  return JSON.stringify(result.Items);
+});
